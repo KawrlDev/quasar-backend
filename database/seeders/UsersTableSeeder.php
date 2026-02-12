1b1b3957c06f2f22db7f1eb5b53299701dd70c6e
KawrlDev <ninjastarz2243@gmail.com>
2026-02-12 10:33:44 +0800

    uppercased name

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -12,7 +12,7 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-            'USERNAME' => 'admin',
+            'USERNAME' => 'ADMIN',
             'PASSWORD' => '$2y$12$JdibzUcHFkBJSqXX0IXFmet5TAcWyGnDveKedqLla19CnPrnj/4mK',
             'REMEMBER_TOKEN' => 'cq2DztGN9gqIoZFV3VUnEEjOKMHlgbIn2I8YeOy7pcLeiHcWvKrNOcl4M54Q',
             'ROLE' => 'ADMIN'
